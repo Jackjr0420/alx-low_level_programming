e338271f0f96ad44e1e3c0a5f53b5367f4016b52
Jackjr <Jackjr0420@gmail.com>
2023-02-24 01:03:07 -0800

    hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main-A program that prints a string using printf() function
  *
- * Return:Always return 0
+ * Return:Always retrun 0
  */
 int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
- }
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
