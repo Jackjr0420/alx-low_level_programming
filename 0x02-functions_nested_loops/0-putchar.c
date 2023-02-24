9ad0cbf0541acdff9b646368baecce7e3f48bf44
Jackjr <Jackjr0420@gmail.com>
2023-02-24 01:41:00 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * main- Prints _putchar
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char s, t, r, i, n, g, b, v, nline;
+
+	s = '_';
+	t = 'p';
+	r = 'u';
+	i = 't';
+	n = 'c';
+	g = 'h';
+	b = 'a';
+	v = 'r';
+	nline = '\n';
+	_putchar(s);
+	_putchar(t);
+	_putchar(r);
+	_putchar(i);
+	_putchar(n);
+	_putchar(g);
+	_putchar(b);
+	_putchar(v);
+	_putchar(nline);
+	return (0);
+}
