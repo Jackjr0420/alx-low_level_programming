b555e01bb0ea777b743bffff71cd1bc6c4176b6c
Jackjr <Jackjr0420@gmail.com>
2023-02-24 01:09:20 -0800

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- * main-A program that prints a string by use of c language
- *
- * Return:Always 0
- */
+*main-A program that prints a string by use of c language
+*
+*Return:Always 0
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0)
+	return (0);
 }
+
